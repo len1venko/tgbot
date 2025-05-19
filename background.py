744bb3c0bb7892422431e55cec2e1532ebbd0de4
len1venko <160732293+len1venko@users.noreply.github.com>
2025-05-19 21:54:14 +0300

    Update background.py

--- background.py
+++ background.py
@@ -1,21 +1,17 @@
 from flask import Flask
-from flask import request
 from threading import Thread
-import time
-import requests
+import os
 
-app = Flask('')
-
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "I'm alive"
-
+    return "I'm alive"
 
 def run():
-  app.run(host='0.0.0.0', port=80)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
