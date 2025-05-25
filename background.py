7053ec73b56ff268e80dd5f6a5a63048f43c59a9
len1venko <160732293+len1venko@users.noreply.github.com>
2025-05-25 21:51:39 +0300

    Update background.py

--- background.py
+++ background.py
@@ -1,38 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from threading import Thread
 import os
 
 app = Flask(__name__)
 
-# Изначальные пороговые значения
-thresholds = {
-    'temp': 25,
-    'humidity': 60
-}
-
 @app.route('/')
 def home():
     return "I'm alive"
-
-@app.route('/set-thresholds', methods=['GET'])
-def set_thresholds():
-    temp = request.args.get('temp', type=float)
-    humidity = request.args.get('humidity', type=float)
-
-    if temp is not None and humidity is not None:
-        thresholds['temp'] = temp
-        thresholds['humidity'] = humidity
-        return jsonify({
-            "status": "success",
-            "message": "Thresholds updated",
-            "thresholds": thresholds
-        }), 200
-    else:
-        return jsonify({"status": "error", "message": "Invalid data"}), 400
-
-@app.route('/get-thresholds', methods=['GET'])
-def get_thresholds():
-    return jsonify(thresholds)
 
 def run():
     port = int(os.environ.get("PORT", 5000))
@@ -40,5 +14,4 @@ def run():
 
 def keep_alive():
     t = Thread(target=run)
-    t.daemon = True
     t.start()
