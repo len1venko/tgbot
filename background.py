f3c561e90318776c0cc6ea7530c505417fefc544
len1venko <160732293+len1venko@users.noreply.github.com>
2025-05-25 20:48:21 +0300

    Update background.py

--- background.py
+++ background.py
@@ -4,9 +4,31 @@ import os
 
 app = Flask(__name__)
 
+# Изначальные пороговые значения
+thresholds = {
+    'temp': 25,
+    'humidity': 60
+}
+
 @app.route('/')
 def home():
     return "I'm alive"
+
+@app.route('/set-thresholds', methods=['GET'])
+def set_thresholds():
+    temp = request.args.get('temp', type=float)
+    humidity = request.args.get('humidity', type=float)
+
+    if temp is not None and humidity is not None:
+        thresholds['temp'] = temp
+        thresholds['humidity'] = humidity
+        return jsonify({"status": "success", "message": "Thresholds updated"}), 200
+    else:
+        return jsonify({"status": "error", "message": "Invalid data"}), 400
+
+@app.route('/get-thresholds', methods=['GET'])
+def get_thresholds():
+    return jsonify(thresholds)
 
 def run():
     port = int(os.environ.get("PORT", 5000))
